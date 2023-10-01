673bbf534354360a6638937eb92a6e887eacc65f
Agasi <agasimkhitaryan93@gmail.com>
2023-10-01 12:33:46 +0300

    Add globals - add player movement - add player health bar

---
+++ scripts/Main.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Main : Node2D
+{
+	private CharacterBody2D Player => GetNode<CharacterBody2D>("/root/Main/Player");
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,37 +3,133 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 100.0f;
+	public const float _health = 100.0f;
+	private string _currentDirection = "none";
+	
+	private AnimatedSprite2D Animation => GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	private Camera2D MainCamera => GetNode<Camera2D>("MainCamera");
+	private ProgressBar HealthBar => GetNode<ProgressBar>("HealthBar");
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	public override void _Ready()
+	{
+		if (!Globals.IsGameStarted)
+		{
+			return;
+		}
+
+		Animation.Play("front_idle");
+	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
-		// Vector2 velocity = Velocity;
-		//
-		// // Add the gravity.
-		// if (!IsOnFloor())
-		// 	velocity.Y += gravity * (float)delta;
-		//
-		// // Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		// 	velocity.Y = JumpVelocity;
-		//
-		// // Get the input direction and handle the movement/deceleration.
-		// // As good practice, you should replace UI actions with custom gameplay actions.
-		// Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		// if (direction != Vector2.Zero)
-		// {
-		// 	velocity.X = direction.X * Speed;
-		// }
-		// else
-		// {
-		// 	velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		// }
-		//
-		// Velocity = velocity;
-		// MoveAndSlide();
+		if (!Globals.IsGameStarted)
+		{
+			return;
+		}
+		
+		PlayerMovement();
+		
+	}
+	
+	private void PlayerMovement()
+	{
+		if (Input.IsActionPressed("ui_right"))
+		{
+			_currentDirection = "right";
+			PlayAnimation(1);
+			Velocity = Velocity with { X = Speed, Y = 0 };
+		}
+		else if (Input.IsActionPressed("ui_left"))
+		{
+			_currentDirection = "left";
+			PlayAnimation(1);
+			Velocity = Velocity with { X = -Speed, Y = 0 };
+		}
+		else if (Input.IsActionPressed("ui_down"))
+		{
+			_currentDirection = "down";
+			PlayAnimation(1);
+			Velocity = Velocity with { X = 0, Y = Speed };
+		}
+		else if (Input.IsActionPressed("ui_up"))
+		{
+			_currentDirection = "up";
+			PlayAnimation(1);
+			Velocity = Velocity with { X = 0, Y = -Speed };
+		}
+		else
+		{
+			// _currentDirection = "none";
+			PlayAnimation(0);
+			Velocity = Velocity with { X = 0, Y = 0 };
+		}
+		
+		MoveAndSlide();
+	}
+	
+	private void PlayAnimation(int movement)
+	{
+		switch (_currentDirection)
+		{
+			case "right":
+				Animation.FlipH = false;
+				if (movement == 1)
+				{
+					Animation.Play("side_walk");
+				}
+				else if (movement == 0)
+				{
+					Animation.Play("side_idle");
+				}
+
+				break;
+			case "left":
+				Animation.FlipH = true;
+				if (movement == 1)
+				{
+					Animation.Play("side_walk");
+				}
+				else if (movement == 0)
+				{
+					Animation.Play("side_idle");
+				}
+
+				break;
+			case "down":
+				Animation.FlipH = true;
+				if (movement == 1)
+				{
+					Animation.Play("front_walk");
+				}
+				else if (movement == 0)
+				{
+					Animation.Play("front_idle");
+				}
+
+				break;
+			case "up":
+				Animation.FlipH = true;
+				if (movement == 1)
+				{
+					Animation.Play("back_walk");
+				}
+				else if (movement == 0)
+				{
+					Animation.Play("back_idle");
+				}
+
+				break;
+			default:
+				Animation.Play("front_idle");
+				break;
+		}
+	}
+	
+	private void UpdateHealth()
+	{
+		HealthBar.Value = _health;
+		// HealthBar.Visible = _health < 100;
 	}
 }
